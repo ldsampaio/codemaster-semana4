901a66323245afa9a270bccda8f0e073285c2d4b
Lucas <ldsampaio@utfpr.edu.br>
2026-08-16 20:14:59 -0300

    Trocando o DynamoDB por lockfile

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "meu-terraform-state-codemaster-lucas"
-    key            = "projeto/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "meu-terraform-state-codemaster-lucas"
+    key          = "projeto/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
